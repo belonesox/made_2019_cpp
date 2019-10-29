5715faf05752c74936498c762fcb98fb136f9b4d
Stas Fomin <stanislav.fomin@gmail.com>
2019-10-29 22:23:46 +0300

    Add random tests, learn a lot about scaling problem of BC

--- 01/test.py
+++ 01/test.py
@@ -7,7 +7,9 @@
 как mock тестируемой программы, и сам python для расчета (нет времени считать самому).
 '''
 from __future__ import print_function
+import sys
 import subprocess
+import random
 
 # Расширяемое количество правильных выражений сделаем потом генерацией вероятностных тестов.
 good_expressions = """
@@ -15,6 +17,8 @@ good_expressions = """
 2 + 3
 5 + -6 
 7+6+7+8-5
+65535*65535-65535*23
+2147483647+2147483647-2147483647*2
 """
 
 #тут конечно да, сложность что bc сьест больше, чем нам надо. Но часть ложных можно и тут.
@@ -37,7 +41,7 @@ fail_expessions = """
 
 def run_mock(expression):
     p = subprocess.Popen("bc", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate((expression + '\n').encode("utf-8"))
+    out, err = p.communicate(("scale=30\n" + expression + '\n').encode("utf-8"))
     # print(expression.encode("utf-8"))
     # print("out", out)
     # print("err", err)
@@ -45,7 +49,7 @@ def run_mock(expression):
     returncode = p.returncode
     result = None
     if not err:
-        result = int(out.strip())     
+        result = int(round(float(out.strip())))     
     else:
         returncode = 1    
     return result, returncode
@@ -54,15 +58,45 @@ def test(func2test=run_mock):
     print("*"*50)
     print("Testing ",  func2test.__name__)
 
-    print("     Testing good expressions ")
-    for expression in good_expressions.strip().split("\n"):
+    print("     Testing random good expressions ")
+
+    def test_good_expression(expression):
         print("     expression  ", expression)
         result, returncode = func2test(expression)
         if returncode != 0:
             print("!"*10, " returncode is not zero! ")
+            return False
         expecting = eval(expression)
-        if result != expecting:
-            print("!"*5, " returned ", result, " expected ", expecting)
+        re_ = int(round(expecting))
+        res_ = int(round(result))
+        if (res_ != re_ and abs(re_)>0) and abs((res_ - re)/re_)>1e-6:
+            print("!"*5, " returned ", res_, " expected ", re_)
+            return False
+        return True
+
+
+    max_integer = 1024
+    # max_integer = 2147483647 — big probablility of overfloating 
+    for k in range(1000): # сколько угодно тестов
+        len_of_expression = random.randint(1, 20)
+        terms_expr  = [random.randint(1, max_integer) for i in range(len_of_expression)]
+        signs_expr  = [random.choice(['', '-']) for i in range(len_of_expression)]
+        ops_expr  = [random.choice(['+', '-', '*', '/']) for i in range(len_of_expression-1)]
+        expr_terms = []
+        for k,(s,t) in enumerate(zip(signs_expr, terms_expr)):
+            expr_terms.append(str(s)+str(t))    
+            if k<len(ops_expr):
+                expr_terms.append(ops_expr[k])    
+            expr_terms.append(' '*random.randint(0, 6))        
+        expr = ''.join(expr_terms)
+        expr = expr.replace('--', '-')
+        if not test_good_expression(expr):
+            return False
+
+    print("     Testing good expressions ")
+    for expression in good_expressions.strip().split("\n"):
+        if not test_good_expression(expression):
+            return False
 
     print("     Testing bad expressions ")
     for expression in fail_expessions.strip().split():
@@ -70,13 +104,18 @@ def test(func2test=run_mock):
         result, returncode = func2test(expression)
         if returncode == 0:
             print("!"*10, " returncode shoud be nonzero! ")
+            return False
 
     print("Testing ",  func2test.__name__, 'done')
     print("-"*50)
+    return True
+
 
 if __name__ == "__main__":
     print("*"*50)
     print("SelfTesting")
     print("-"*50)
-    test()
+    if not test():
+        sys.exit(-1)
+    pass        
 
