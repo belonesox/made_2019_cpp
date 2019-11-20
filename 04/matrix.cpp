5b297771ac78902669c17df381c1a03a0257b2ab
Stas Fomin <stanislav.fomin@gmail.com>
2019-11-20 18:16:41 +0300

    Tests looks OK

--- 04/matrix.cpp
+++ 04/matrix.cpp
@@ -1,8 +1,13 @@
 #include <cstring>
 #include <iostream>
+// Используются только для тестов, boost можно считать стандартной библитекой,
+// catch2 принес с собой.
+#include <boost/tokenizer.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
 /**
  * @brief Основной класс реализации плотной матрицы, с построчным хранением
  *
@@ -53,6 +58,7 @@ class Matrix {
 
     ~Matrix() {
         delete[] m_buffer;
+        m_buffer = nullptr;
     };
 
     int get_rows_num() const {
@@ -75,8 +81,9 @@ class Matrix {
         if (m_rowsnum != matrix.m_rowsnum || m_colsnum != matrix.m_colsnum)
             return false;
 
-        return std::memcmp(m_buffer, m_buffer,
-                           m_colsnum * m_rowsnum * sizeof(int)) == 0;
+        auto res = std::memcmp(m_buffer, matrix.m_buffer,
+                               m_colsnum * m_rowsnum * sizeof(int));
+        return res == 0;
     }
 
     /**
@@ -131,29 +138,136 @@ class Matrix {
     }
 };
 
-Matrix &load_matrix_from_string(std::string &str) {
+/**
+ * @brief Для тестов удобней использовать текстовые представления строк,
+ *  но чтобы не замусоривать требуемый класс еще одним конструктором, отдельная
+ * прокси функция. Плюс целевой класс не тянет зависимостей от boost, который
+ * тут хотелось попробовать.
+ *
+ * @param str
+ * @return Matrix*
+ */
+Matrix *load_matrix_from_string(std::string str) {
+    boost::char_separator<char> rowsep("\n");
+    boost::char_separator<char> colsep(" ");
+    boost::tokenizer<boost::char_separator<char>> lines(str, rowsep);
+    uint rows_num = 0;
+    uint cols_num = 0;
+    BOOST_FOREACH (std::string line, lines) {
+        uint cols_num_ = 0;
+        auto line_ok_ = false;
+        boost::tokenizer<boost::char_separator<char>> cols(line, colsep);
+        BOOST_FOREACH (std::string col, cols) {
+            line_ok_ = true;
+            cols_num_++;
+        }
+        if (line_ok_) rows_num++;
+        cols_num = std::max(cols_num, cols_num_);
+    }
+
+    Matrix *pmat = new Matrix(rows_num, cols_num);
+
+    uint i = 0;
+    BOOST_FOREACH (std::string line, lines) {
+        uint j = 0;
+        boost::tokenizer<boost::char_separator<char>> cols(line, colsep);
+        BOOST_FOREACH (std::string col, cols) {
+            int val = boost::lexical_cast<int>(col);
+            (*pmat)[i][j++] = val;
+        }
+        i++;
+    }
+
+    return pmat;
 }
 
 TEST_CASE("Горячий тест — что сломалось в последний раз", "[matrix]") {
     Matrix mat(3, 5);
-    const char *mat1_str = R"0x1.tv(
+    const char *mat3x5_str = R"0x1.tv(
 1 0 0 0 4
 0 1 0 0 3
 0 2 3 0 5 
     )0x1.tv";
 
-    const char *mat2_str = R"0x1.tv(
-1 0 0 0 
-0 1 0 0 
-0 2 3 0  
+    Matrix mat3x5(*load_matrix_from_string(mat3x5_str));
+    Matrix mat3x5_01(*load_matrix_from_string(mat3x5_str));
+    REQUIRE(mat3x5 == mat3x5_01);
+    mat3x5_01 *= 7;
+    auto wtf = mat3x5_01[0][0];
+    auto wtf1 = mat3x5_01[2][1];
+}
+
+TEST_CASE("Тесты на равенство и умножение", "[matrix]") {
+    Matrix mat(3, 5);
+    const char *mat3x5_str = R"0x1.tv(
+1 0 0 0 4
+0 1 0 0 3
+0 2 3 0 5 
     )0x1.tv";
 
-    REQUIRE(1);
+    Matrix mat3x5(*load_matrix_from_string(mat3x5_str));
+    Matrix mat3x5_same(*load_matrix_from_string(mat3x5_str));
 
-    //     SECTION("Проверяем только строки") {
-    //         testout.str("");
-    //         tok.register_onString(testString_);
-    //         tok.parse(wtf_text);
-    //         REQUIRE(R"0x1.tv()0x1.tv" == testout.str());
-    //     }
+    const char *mat3x4_str = R"0x1.tv(
+    1 0 0 0
+    0 1 0 0
+    0 2 3 0
+        )0x1.tv";
+    Matrix mat3x4(*load_matrix_from_string(mat3x4_str));
+
+    REQUIRE(5 == mat3x5.get_columns_num());
+    REQUIRE(3 == mat3x5.get_rows_num());
+
+    REQUIRE(mat3x5 == mat3x5);
+    REQUIRE(mat3x5 == mat3x5_same);
+    mat3x5_same[2][3] = 1;
+    REQUIRE(mat3x5 != mat3x5_same);
+    REQUIRE(mat3x4 != mat3x5);
+
+    Matrix mat3x5_01(*load_matrix_from_string(mat3x5_str));
+    REQUIRE(mat3x5 == mat3x5_01);
+    mat3x5_01 *= 7;
+    REQUIRE(7 == mat3x5_01[0][0]);
+    REQUIRE(14 == mat3x5_01[2][1]);
+    REQUIRE(21 == mat3x5_01[2][2]);
+
+    REQUIRE(mat3x5 != mat3x5_01);
+
+    mat3x5 *= 0;
+    mat3x5_01 *= 0;
+    REQUIRE(mat3x5 == mat3x5_01);
+    REQUIRE(1);
+}
+
+TEST_CASE("Тесты умножение", "[matrix]") {
+    Matrix mat(3, 5);
+    const char *mat3x5_str = R"0x1.tv(
+1 -1 2 -2 3
+0 8 13 -45 1
+-3 -4 -5 -6 0
+    )0x1.tv";
+
+    const char *mat3x5_times_minus7_str = R"0x1.tv(
+-7 7 -14 14 -21
+0 -56 -91 315 -7
+21 28 35 42 0
+    )0x1.tv";
+
+    Matrix mat3x5(*load_matrix_from_string(mat3x5_str));
+    Matrix mat3x5_times_minus7(
+        *load_matrix_from_string(mat3x5_times_minus7_str));
+
+    REQUIRE(mat3x5 != mat3x5_times_minus7);
+    mat3x5 *= -7;
+
+    REQUIRE(mat3x5 == mat3x5_times_minus7);
+
+    mat3x5 *= -2;
+    REQUIRE(mat3x5 != mat3x5_times_minus7);
+
+    mat3x5 *= -0;
+    mat3x5_times_minus7 *= 0;
+    REQUIRE(mat3x5 == mat3x5_times_minus7);
+
+    REQUIRE(1);
 }
